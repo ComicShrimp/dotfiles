fa0cf611b71797867de7481c98ca32c0f25a0d9b
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-04-11 10:18:25 -0300

    feat(shell): add spaceship config

---
+++ .config/spaceship.zsh
@@ -0,0 +1,15 @@
+SPACESHIP_PROMPT_ORDER=(
+  venv          # Virtual Environment
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  exec_time     # Execution time
+  exit_code     # Exit code section
+  char          # Prompt character
+)
+SPACESHIP_PROMPT_ASYNC=false
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
