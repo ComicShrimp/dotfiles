af60086c3f481cedba175bee3b4c54bd39a42007
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-04-15 11:01:00 -0300

    feat(shell): add aliases

--- .config/aliases/aliases.sh
+++ .config/aliases/aliases.sh
@@ -1,7 +1,14 @@
 # Pacman
 
+alias paci="sudo pacman -S"   # Install package
 alias pacu="sudo pacman -Syu" # Update system
+
+# Paru
+
+alias parui="paru -S"   # Install package
+alias paruu="paru -Syu" # Update system
 
 # Yay
 
+alias yayi="yay -S"   # Install package
 alias yayu="yay -Syu" # Update system
