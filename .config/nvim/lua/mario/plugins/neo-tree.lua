8510200c31f4b8e0154bfd8c5fb4cdb2d91b0948
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-04-15 10:37:22 -0300

    feat(nvim): configure neo-tree

--- .config/nvim/lua/mario/plugins/neo-tree.lua
+++ .config/nvim/lua/mario/plugins/neo-tree.lua
@@ -10,4 +10,11 @@ return {
 		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
 	end,
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+      },
+    },
+  },
 }
