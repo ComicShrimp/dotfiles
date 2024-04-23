a0e8ad83d7ea787df0c8803a2fdc28daf1b23957
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-04-23 16:56:46 -0300

    chore(nvim): add wichkey description

--- .config/nvim/lua/mario/plugins/nvim_tmux_navigation.lua
+++ .config/nvim/lua/mario/plugins/nvim_tmux_navigation.lua
@@ -2,9 +2,9 @@ return {
   "alexghergh/nvim-tmux-navigation",
   config = function()
     require("nvim-tmux-navigation").setup({})
-    vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", {})
-    vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", {})
-    vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", {})
-    vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", {})
+    vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { desc = "Navigate to the 'Left' panel" })
+    vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { desc = "Navigate to the 'Down' panel" })
+    vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { desc = "Navigate to the 'Up' panel" })
+    vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { desc = "Navigate to the 'Right' panel" })
   end,
 }
