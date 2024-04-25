b4757dfd5a18f447165dbc5a3b69d8370f6ec1cc
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-04-25 15:07:28 -0300

    chore(nvim): configure neo-tree

--- .config/nvim/lua/mario/plugins/neo-tree.lua
+++ .config/nvim/lua/mario/plugins/neo-tree.lua
@@ -6,8 +6,30 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
-    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
-    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-  end,
+  opts = {
+    close_if_last_window = true,
+    window = {
+      position = "left",
+    },
+    filesystem = {
+      follow_current_file = {
+        enabled = true,
+        leave_dirs_open = false,
+      },
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+        },
+        always_show = {
+          ".env",
+        },
+      },
+    },
+  },
+  -- config = function()
+  --   vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", {})
+  --   vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+  -- end,
 }
