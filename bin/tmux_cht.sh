4357df74e7c1054b1719af38cbc34d0e4daa1e25
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-04-30 14:30:15 -0300

    wip(script): script for cht.sh

--- bin/tmux_cht.sh
+++ bin/tmux_cht.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-echo "Hello World!"
+langs=(
+  "python"
+  "rust"
+)
+
+commands=(
+  "git"
+  "stow"
+)
+
+selected=$({ printf "%s\n" "${langs[@]}"; printf "%s\n" "${commands[@]}"; } | fzf)
+
+if [[ $langs == $selected ]]; then
+  echo "You selected a language"
+elif [[ $commands == $selected ]]; then
+  echo "You selected a command"
+fi
+
+read -p "Enter Query: " query
+
+echo "Hello World!" | bat
+while [ : ]; do sleep 1; done
