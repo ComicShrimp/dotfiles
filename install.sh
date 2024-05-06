50c6c043cd7896d501222875c672f257e4033740
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-05-05 22:44:38 -0300

    wip(install): instalation script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+packages=(
+  "stow"
+  "git"
+  "zsh"
+  "neovim"
+  "alacritty"
+  "ripgrep"
+  "starship"
+  "unzip"
+  "lazygit"
+  "tmux" 
+  "fzf"
+)
+
+echo "Install following packages ${packages[@]}"
