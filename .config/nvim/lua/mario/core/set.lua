421ab741d37effcd226aa925a952d3ac61d68c21
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-05-06 14:52:25 -0300

    feat(nvim): add todo comments

--- .config/nvim/lua/mario/core/set.lua
+++ .config/nvim/lua/mario/core/set.lua
@@ -21,10 +21,20 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = "split"
+vim.opt.hlsearch = true
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+vim.opt.clipboard = "unnamedplus"
