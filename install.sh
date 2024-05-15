819a31c28ae3db65d80f103cf653ffc3e144d342
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-05-14 23:01:27 -0300

    wip(installer): package files

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-packages=(
-  "stow"
-  "git"
-  "zsh"
-  "neovim"
-  "alacritty"
-  "ripgrep"
-  "starship"
-  "unzip"
-  "lazygit"
-  "tmux" 
-  "fzf"
-)
+PACKAGES_FILE="./scripts/packages"
 
-echo "Install following packages ${packages[@]}"
+packages=$(cat $PACKAGES_FILE )
+
+
+echo "Installing required packages..."
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Mac OS detected. Using homebrew"
+    xargs brew install < "$PACKAGES_FILE"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux detected. Using pacman"
+    xargs sudo pacman -S < "$PACKAGES_FILE"
+else
+    echo "Unknown operating system."
+fi
+
