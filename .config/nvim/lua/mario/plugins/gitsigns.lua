e94096560d23c426e953e323a0661ab7ebc5e677
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-06-13 09:04:53 -0300

    feat(nvim): add line git blame

--- .config/nvim/lua/mario/plugins/gitsigns.lua
+++ .config/nvim/lua/mario/plugins/gitsigns.lua
@@ -1,6 +1,10 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    require("gitsigns").setup()
+    local gitsign = require("gitsigns")
+
+    gitsign.setup()
+
+    vim.keymap.set("n", "<leader>b", gitsign.toggle_current_line_blame, { desc = "Toggle current line blame" })
   end,
 }
