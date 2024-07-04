f7209735e6c398d908aa46ac883d7ae51f898f8c
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-07-03 23:42:19 -0300

    fix(nvim): trouble modes

--- .config/nvim/lua/mario/plugins/trouble.lua
+++ .config/nvim/lua/mario/plugins/trouble.lua
@@ -11,21 +11,18 @@ return {
     local trouble = require("trouble")
 
     -- Lua
-    vim.keymap.set("n", "<leader>xx", function()
-      trouble.toggle()
-    end, { desc = "Toggle trouble" })
-    vim.keymap.set("n", "<leader>xw", function()
-      trouble.toggle("workspace_diagnostics")
-    end, { desc = "Toggle Workspace Diagnostics" })
     vim.keymap.set("n", "<leader>xd", function()
-      trouble.toggle("document_diagnostics")
-    end, { desc = "Toggle Document Diagnostics" })
+      trouble.toggle("diagnostics")
+    end, { desc = "Toggle Diagnostics" })
+
     vim.keymap.set("n", "<leader>xq", function()
       trouble.toggle("quickfix")
     end, { desc = "Toggle Quickfix" })
+
     vim.keymap.set("n", "<leader>xl", function()
       trouble.toggle("loclist")
     end, { desc = "Toggle loclist" })
+
     vim.keymap.set("n", "gR", function()
       trouble.toggle("lsp_references")
     end, { desc = "Toggle LSP References" })
