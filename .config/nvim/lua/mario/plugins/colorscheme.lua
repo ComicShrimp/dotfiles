b85a3121c8c64d0cba10716ec8612161cff85608
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-07-17 23:08:25 -0300

    chore(nvim): return theme to catppuccin

--- .config/nvim/lua/mario/plugins/colorscheme.lua
+++ .config/nvim/lua/mario/plugins/colorscheme.lua
@@ -1,21 +1,11 @@
 return {
-
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      vim.cmd.colorscheme("tokyonight-storm")
-    end,
-  },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    -- config = function()
-    --   vim.cmd.colorscheme("catppuccin-mocha")
-    -- end,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
     opts = {
       integrations = {
         cmp = true,
