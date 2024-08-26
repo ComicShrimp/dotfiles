cb2273b5de5fcd2beebfd1247a0e3f4cbacbb332
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-08-26 14:01:16 -0300

    chore(wezterm): add fullscreen startup

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+local mux = wezterm.mux
 
 config.color_scheme = "Catppuccin Mocha"
 
@@ -11,13 +12,12 @@ config.enable_tab_bar = false
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 3000
 config.native_macos_fullscreen_mode = true
+config.window_decorations = "RESIZE"
 
-local mux = wezterm.mux
-
-wezterm.on("gui-startup", function()
-  local _, pane, window = mux.spawn_window({})
-  local gui_window = window:gui_window()
-  gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
+-- Start in fullscreen
+wezterm.on("gui-startup", function(cmd)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():toggle_fullscreen()
 end)
 
 -- and finally, return the configuration to wezterm
