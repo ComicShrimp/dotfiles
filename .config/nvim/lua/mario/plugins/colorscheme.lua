76a3ad3c92820d90bd585f9ec48dcd1fc368e822
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-09-30 14:42:43 -0300

    feat(nvim): add new plugins

--- .config/nvim/lua/mario/plugins/colorscheme.lua
+++ .config/nvim/lua/mario/plugins/colorscheme.lua
@@ -1,5 +1,11 @@
 return {
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
