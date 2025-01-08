8a6464ef8388e6ef812321f70edc9d018f3375e0
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-01-08 14:19:59 -0300

    chore(nvim): change telescope to be on top

--- .config/nvim/lua/mario/plugins/telescope.lua
+++ .config/nvim/lua/mario/plugins/telescope.lua
@@ -24,6 +24,10 @@ return {
           -- `hidden = true` is not supported in text grep commands.
           vimgrep_arguments = vimgrep_arguments,
           path_display = { "smart" },
+          layout_config = {
+            prompt_position = "top",
+          },
+          sorting_strategy = "ascending",
         },
         pickers = {
           find_files = {
