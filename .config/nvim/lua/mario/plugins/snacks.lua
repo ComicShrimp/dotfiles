99aab5560c40e0b74a8332ec2b6b46524576300c
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-01-08 12:19:01 -0300

    refactor(nvim): remove plugins in favor of snacks.nvim

--- .config/nvim/lua/mario/plugins/snacks.lua
+++ .config/nvim/lua/mario/plugins/snacks.lua
@@ -11,7 +11,21 @@ return {
       },
     },
     dashboard = { enabled = true },
-    -- statuscolumn = { enabled = true },
+    input = { enabled = true },
+    words = { enabled = true },
+    statuscolumn = {
+      left = { "mark", "sign" }, -- priority of signs on the left (high to low)
+      right = { "fold", "git" }, -- priority of signs on the right (high to low)
+      folds = {
+        open = false, -- show open fold icons
+        git_hl = false, -- use Git Signs hl for fold icons
+      },
+      git = {
+        -- patterns to match Git signs
+        patterns = { "GitSign", "MiniDiffSign" },
+      },
+      refresh = 50, -- refresh at most every 50ms
+    },
   },
   keys = {
     {
@@ -20,6 +34,20 @@ return {
         Snacks.lazygit()
       end,
       desc = "Open Lazygit",
+    },
+    {
+      "]]",
+      function()
+        Snacks.words.jump(1, true)
+      end,
+      desc = "Jump to next reference",
+    },
+    {
+      "[[",
+      function()
+        Snacks.words.jump(-1, true)
+      end,
+      desc = "Jump to previous reference",
     },
   },
 }
