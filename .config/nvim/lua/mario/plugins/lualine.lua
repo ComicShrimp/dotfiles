baf2ce669eb09b2a2e54bf42d33fa11fdeee9788
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-04-01 22:06:31 -0300

    feat(nvim): show macro recording

--- .config/nvim/lua/mario/plugins/lualine.lua
+++ .config/nvim/lua/mario/plugins/lualine.lua
@@ -11,6 +11,17 @@ return {
       sections = {
         lualine_c = {
           { "filename", path = 1 },
+          {
+            "macro",
+            fmt = function()
+              local reg = vim.fn.reg_recording()
+              if reg ~= "" then
+                return "Recording @" .. reg
+              end
+              return nil
+            end,
+            draw_empty = false,
+          },
         },
       },
       extensions = { "neo-tree", "lazy", "trouble", "mason", "oil" },
