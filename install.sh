cdfa69a637e368f7b4e26ad2c4ca2a1709931b28
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-04-11 08:24:53 -0300

    wip(install): revamp install script

--- install.sh
+++ install.sh
@@ -1,21 +1,32 @@
 #!/bin/sh
 
-PACKAGES_FILE="./scripts/packages"
+# Print the logo
+print_logo() {
+    cat << "EOF"
+    Teste de Logo
+EOF
+}
 
-packages=$(cat $PACKAGES_FILE )
+# Clear screen and show logo
+clear
+print_logo
 
-
-echo "Installing required packages..."
-
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "Mac OS detected. Using homebrew"
-    xargs brew install < "$PACKAGES_FILE"
-elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "Linux detected. Using pacman"
-    xargs sudo pacman -S < "$PACKAGES_FILE"
-else
-    echo "Unknown operating system."
-fi
-
-# Add new themes to bat
-bat cache --build
+# PACKAGES_FILE="./scripts/packages"
+#
+# packages=$(cat $PACKAGES_FILE )
+#
+#
+# echo "Installing required packages..."
+#
+# if [[ "$OSTYPE" == "darwin"* ]]; then
+#     echo "Mac OS detected. Using homebrew"
+#     xargs brew install < "$PACKAGES_FILE"
+# elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#     echo "Linux detected. Using pacman"
+#     xargs sudo pacman -S < "$PACKAGES_FILE"
+# else
+#     echo "Unknown operating system."
+# fi
+#
+# # Add new themes to bat
+# bat cache --build
