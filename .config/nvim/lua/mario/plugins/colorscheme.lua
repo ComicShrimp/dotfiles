b1e0bdb07241a0e6c2b41214ffc2e6fa279d2272
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-07-29 08:53:44 -0300

    chore(aerospace): remove unused workspaces

--- .config/nvim/lua/mario/plugins/colorscheme.lua
+++ .config/nvim/lua/mario/plugins/colorscheme.lua
@@ -3,6 +3,9 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("tokyonight-night")
+    end,
     opts = {},
   },
   { "EdenEast/nightfox.nvim", priority = 1000, opts = {} },
@@ -14,9 +17,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
-    end,
     opts = {
       integrations = {
         cmp = true,
