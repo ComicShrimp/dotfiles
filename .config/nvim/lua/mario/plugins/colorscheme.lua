22c03a1ffa76810dcfb33104da2f4cf69b8047bd
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-08-05 08:25:17 -0300

    feat(nvim): catpuccin-mocha config

--- .config/nvim/lua/mario/plugins/colorscheme.lua
+++ .config/nvim/lua/mario/plugins/colorscheme.lua
@@ -3,9 +3,6 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("tokyonight-night")
-    end,
     opts = {},
   },
   { "EdenEast/nightfox.nvim", priority = 1000, opts = {} },
@@ -17,14 +14,23 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+      local sign = vim.fn.sign_define
+
+      sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
+      sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
+      sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
+    end,
     opts = {
       integrations = {
+        dap = true,
+        dap_ui = true,
         cmp = true,
         blink_cmp = true,
         dashboard = true,
         gitsigns = true,
         headlines = true,
-        illuminate = true,
         dropbar = {
           enabled = false,
           color_mode = true, -- enable color for kind's texts, not just kind's icons
@@ -35,11 +41,22 @@ return {
         markdown = true,
         native_lsp = {
           enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+            ok = { "italic" },
+          },
           underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+            ok = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
           },
         },
         neotree = true,
@@ -49,6 +66,10 @@ return {
         treesitter_context = true,
         which_key = true,
         harpoon = true,
+        illuminate = {
+          enabled = true,
+          lsp = false,
+        },
       },
     },
   },
