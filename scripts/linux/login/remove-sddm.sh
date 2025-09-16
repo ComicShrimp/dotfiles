33375abbf07048e1ca748c4d9600b1f64506e12c
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-09-16 11:54:04 -0300

    fix(install): remove sddm package

--- scripts/linux/login/remove-sddm.sh
+++ scripts/linux/login/remove-sddm.sh
@@ -2,3 +2,8 @@
 
 # Disable the SDDM service to prevent start at boot
 sudo systemctl disable sddm.service
+
+# Remove the SDDM package
+sudo pacman -Rs sddm
+
+echo "SDDM has been stopped, disabled, and removed."
