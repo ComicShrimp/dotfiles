3fe33711ec1c29af45a7e8a38b14c55964222f37
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2025-12-08 10:07:18 -0300

    refactor(nvim): colorscheme

--- .config/nvim/lua/mario/plugins/colorscheme.lua
+++ .config/nvim/lua/mario/plugins/colorscheme.lua
@@ -4,24 +4,17 @@ return {
     lazy = false,
     priority = 1000,
     opts = {},
-  },
-  { "EdenEast/nightfox.nvim", priority = 1000, opts = {} },
-  {
-    "rmehri01/onenord.nvim",
-    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("tokyonight")
+    end,
   },
   {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("catppuccin-mocha")
-      local sign = vim.fn.sign_define
-
-      sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = "" })
-      sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = "" })
-      sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = "" })
-    end,
+    -- config = function()
+    --   vim.cmd.colorscheme("catppuccin-mocha")
+    -- end,
     opts = {
       integrations = {
         dap = true,
