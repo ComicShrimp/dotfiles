759f3874c2ae0a5a56dce807f4e30ad2e24f0746
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-01-16 16:01:01 -0300

    chore(nvim): disable ghost text

--- .config/nvim/lua/mario/plugins/completions.lua
+++ .config/nvim/lua/mario/plugins/completions.lua
@@ -56,7 +56,7 @@ return {
       documentation = { auto_show = true, auto_show_delay_ms = 500 },
 
       -- Display a preview of the selected item on the current line
-      ghost_text = { enabled = true },
+      ghost_text = { enabled = false },
     },
 
     -- Default list of enabled providers defined so that you can extend it
