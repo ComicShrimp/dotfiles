6e786a4d488b0b24a7cc3f0085d5ae7ce5b72e0d
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-01-26 10:33:11 -0300

    feat(nvim): configure DAP for node

--- .config/nvim/lua/mario/plugins/dap.lua
+++ .config/nvim/lua/mario/plugins/dap.lua
@@ -31,6 +31,17 @@ return {
             },
           },
         },
+        ["node"] = function(cb, config)
+          if config.type == "node" then
+            config.type = "pwa-node"
+          end
+          local a = dap.adapters["pwa-node"]
+          if type(a) == "function" then
+            a(cb, config)
+          else
+            cb(a)
+          end
+        end,
       }
 
       -- DAP Configurations
@@ -39,10 +50,14 @@ return {
           {
             type = "pwa-node",
             request = "attach",
-            name = "Auto Attach",
-            cwd = vim.fn.getcwd(),
-            protocol = "inspector",
+            name = "Attach to Node.js",
             port = 9229,
+            address = "localhost",
+            localRoot = vim.fn.getcwd(),
+            remoteRoot = "/usr/src/app",
+            cwd = vim.fn.getcwd(),
+            sourceMaps = true,
+            protocol = "inspector",
           },
           {
             type = "pwa-node",
@@ -61,6 +76,9 @@ return {
         },
       }
 
+      -- Share TypeScript config with JavaScript
+      dap.configurations.javascript = dap.configurations.typescript
+
       -- Setup adapters
       for name, config in pairs(adapters) do
         dap.adapters[name] = config
@@ -71,23 +89,66 @@ return {
         dap.configurations[lang] = config
       end
 
-      -- Share TypeScript config with JavaScript
-      dap.configurations.javascript = dap.configurations.typescript
-
-      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-      vim.keymap.set("n", "<space>dr", dap.run_to_cursor)
-
       -- Eval var under cursor
       vim.keymap.set("n", "<space>?", function()
         require("dapui").eval(nil, { enter = true })
       end)
 
-      vim.keymap.set("n", "<F1>", dap.continue)
-      vim.keymap.set("n", "<F2>", dap.step_into)
-      vim.keymap.set("n", "<F3>", dap.step_over)
-      vim.keymap.set("n", "<F4>", dap.step_out)
-      vim.keymap.set("n", "<F5>", dap.step_back)
-      vim.keymap.set("n", "<F13>", dap.restart)
+      vim.keymap.set("n", "<leader>db", function()
+        require("dap").toggle_breakpoint()
+      end, { desc = "Toggle Breakpoint" })
+      vim.keymap.set("n", "<leader>dB", function()
+        require("dap").list_breakpoints()
+        vim.cmd("copen")
+      end, { desc = "List Breakpoints" })
+      vim.keymap.set("n", "<leader>dc", function()
+        require("dap").continue()
+      end, { desc = "Run/Continue" })
+      vim.keymap.set("n", "<leader>dC", function()
+        require("dap").run_to_cursor()
+      end, { desc = "Run to Cursor" })
+      vim.keymap.set("n", "<leader>dg", function()
+        require("dap").goto_()
+      end, { desc = "Go to Line (No Execute)" })
+      vim.keymap.set("n", "<leader>di", function()
+        require("dap").step_into()
+      end, { desc = "Step Into" })
+      vim.keymap.set("n", "<leader>dj", function()
+        require("dap").down()
+      end, { desc = "Down" })
+      vim.keymap.set("n", "<leader>dk", function()
+        require("dap").up()
+      end, { desc = "Up" })
+      vim.keymap.set("n", "<leader>dl", function()
+        require("dap").run_last()
+      end, { desc = "Run Last" })
+      vim.keymap.set("n", "<leader>do", function()
+        require("dap").step_out()
+      end, { desc = "Step Out" })
+      vim.keymap.set("n", "<leader>dO", function()
+        require("dap").step_over()
+      end, { desc = "Step Over" })
+      vim.keymap.set("n", "<leader>dP", function()
+        require("dap").pause()
+      end, { desc = "Pause" })
+      vim.keymap.set("n", "<leader>dr", function()
+        require("dap").repl.toggle()
+      end, { desc = "Toggle REPL" })
+      vim.keymap.set("n", "<leader>ds", function()
+        require("dap").session()
+      end, { desc = "Session" })
+      vim.keymap.set("n", "<leader>dt", function()
+        require("dap").terminate()
+        vim.defer_fn(function()
+          require("dapui").close({})
+        end, 100)
+      end, { desc = "Terminate" })
+      vim.keymap.set("n", "<leader>dw", function()
+        require("dap.ui.widgets").hover()
+      end, { desc = "DAP Widgets" })
+      vim.keymap.set("n", "<leader>du", function()
+        require("dapui").toggle({})
+      end, { desc = "Dap UI" })
 
       dap.listeners.before.attach.dapui_config = function()
         ui.open()
@@ -101,6 +162,12 @@ return {
       dap.listeners.before.event_exited.dapui_config = function()
         ui.close()
       end
+
+      -- Setup Breakpoint text
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "●", -- a large dot
+        texthl = "DapBreakpointSign",
+      })
     end,
   },
 }
