ac1356c3bb10f3467c07c323f23317642845e7a7
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-03-02 11:29:04 -0300

    feat(nvim): add `99.nvim` AI plugin

--- .config/nvim/lua/mario/plugins/completions.lua
+++ .config/nvim/lua/mario/plugins/completions.lua
@@ -3,6 +3,15 @@ return {
   event = { "InsertEnter", "CmdlineEnter" },
   dependencies = {
     {
+      "saghen/blink.compat",
+      -- use v2.* for blink.cmp v1.*
+      version = "2.*",
+      -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+      lazy = true,
+      -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+      opts = {},
+    },
+    {
       "L3MON4D3/LuaSnip",
       -- follow latest release.
       version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
