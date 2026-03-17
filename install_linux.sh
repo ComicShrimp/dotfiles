5ec52f233830ce80c1ce47d7f5556a9362040684
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-03-17 08:24:16 -0300

    refactor(install): change filenames

--- install_linux.sh
+++ install_linux.sh
@@ -7,15 +7,15 @@ set -e # Exit immediately if a command exits with a non-zero status
 
 # Install scripts
 
-source $LINUX_INSTALL_FOLDER/1-yay.sh # Yay install
-source $LINUX_INSTALL_FOLDER/2-pacman.sh # Pacman Conf
-source $LINUX_INSTALL_FOLDER/3-development.sh # bluetooth install
-source $LINUX_INSTALL_FOLDER/4-docker.sh # Docker Install
-source $LINUX_INSTALL_FOLDER/5-terminal.sh # General tools used in terminal Install
-source $LINUX_INSTALL_FOLDER/6-firewall.sh # Firewall Install
-source $LINUX_INSTALL_FOLDER/7-fonts.sh # Fonts Install
-source $LINUX_INSTALL_FOLDER/8-programs.sh # programs Install
-source $LINUX_INSTALL_FOLDER/9-power.sh # Power Profile Install
+source $LINUX_INSTALL_FOLDER/01-yay.sh # Yay install
+source $LINUX_INSTALL_FOLDER/02-pacman.sh # Pacman Conf
+source $LINUX_INSTALL_FOLDER/03-development.sh # bluetooth install
+source $LINUX_INSTALL_FOLDER/04-docker.sh # Docker Install
+source $LINUX_INSTALL_FOLDER/05-terminal.sh # General tools used in terminal Install
+source $LINUX_INSTALL_FOLDER/06-firewall.sh # Firewall Install
+source $LINUX_INSTALL_FOLDER/07-fonts.sh # Fonts Install
+source $LINUX_INSTALL_FOLDER/08-programs.sh # programs Install
+source $LINUX_INSTALL_FOLDER/09-power.sh # Power Profile Install
 
 # Change default terminal
 chsh -s $(which zsh)
