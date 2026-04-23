1cf83a84f002868b25360788a8ee07e638a53e1a
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-04-22 22:29:55 -0300

    fix(nvim): `dropbar.nvim` creating header in `neotree`

--- .config/nvim/lua/mario/plugins/dropbar.lua
+++ .config/nvim/lua/mario/plugins/dropbar.lua
@@ -5,4 +5,13 @@ return {
     "nvim-telescope/telescope-fzf-native.nvim",
     build = "make",
   },
+  opts = {
+    bar = {
+      enable = function(buf, win, _)
+        return vim.bo[buf].buftype == ""
+          and vim.fn.win_gettype(win) == ""
+          and vim.bo[buf].filetype ~= "neo-tree"
+      end,
+    },
+  },
 }
