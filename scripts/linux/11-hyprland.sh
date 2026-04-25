4b329994d24ddcb6d8cb360b9184f07b26bb697a
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-04-24 21:10:50 -0300

    feat(hypr): initial hyprland config

---
+++ scripts/linux/11-hyprland.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+yay -S --noconfirm --needed \
+  eza zoxide impala inetutils alacritty \
+  waybar rofi \
+  hyprpaper hypridle hyprlock hyprshutdown \
+  xdg-desktop-portal-hyprland \
+  brightnessctl playerctl
