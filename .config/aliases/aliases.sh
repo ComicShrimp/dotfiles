df8635bcd1f8ffc0acd0e4bc88cd698b63e92aea
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2026-06-18 21:44:28 -0300

    feat(alias): add aliases for nix

--- .config/aliases/aliases.sh
+++ .config/aliases/aliases.sh
@@ -3,18 +3,22 @@ alias ls='ls --color'
 
 # Pacman
 
-alias paci="sudo pacman -S"   # Install package
-alias pacu="sudo pacman -Syu" # Update system
+# alias paci="sudo pacman -S"   # Install package
+# alias pacu="sudo pacman -Syu" # Update system
 
 # Paru
 
-alias parui="paru -S"   # Install package
-alias paruu="paru -Syu" # Update system
+# alias parui="paru -S"   # Install package
+# alias paruu="paru -Syu" # Update system
 
 # Yay
 
-alias yayi="yay -S"   # Install package
-alias yayu="yay -Syu" # Update system
+# alias yayi="yay -S"   # Install package
+# alias yayu="yay -Syu" # Update system
+
+# Nix
+alias nfu="nix flake update"
+alias nfs="sudo nixos-rebuild switch --flake"
 
 # Flatpak
 alias fpu="flatpak update"
